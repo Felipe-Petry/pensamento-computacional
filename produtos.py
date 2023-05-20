dd72248341ea72664b93ea671cf8ae6c752faf59
Felipe-Petry <125224343+Felipe-Petry@users.noreply.github.com>
2023-05-19 21:34:15 -0300

    Create produtos.py

---
+++ produtos.py
@@ -0,0 +1,13 @@
+def prod(x , y , z):
+    prod = x * y * z
+
+    return prod
+
+def main():    
+    a = int(input("digite um numero"))
+    b = int(input("digite um numero"))
+    c = int(input("digite um numero"))
+    
+    p = prod(a , b, c)
+    print(p)
+main()    
